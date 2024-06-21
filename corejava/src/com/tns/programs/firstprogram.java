820b0a93b9f9e9c7853a0c65f6937e66d6716471
user <user@DESKTOP-8LJOACU>
2024-06-21 17:25:56 +0530

    commit message

---
+++ corejava/src/com/tns/programs/firstprogram.java
@@ -0,0 +1,5 @@
+package com.tns.programs;
+
+public class firstprogram {
+
+}
---
+++ corejava/src/com/tns/programs/secondprogram.java
@@ -0,0 +1,10 @@
+package com.tns.programs;
+
+public class secondprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
